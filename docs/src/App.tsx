d081faf0853c1d28d32c2e81ec0736cca249a1c4
Cavasier Morris <cavamorr@gmail.com>
2026-09-11 12:02:10 -0500

    full revert

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -8,7 +8,7 @@ import Projects from './pages/Projects.tsx';
 
 export default function App() {
   return (
-    <BrowserRouter basename={"/presscentra/"}>
+    <BrowserRouter basename={"/PressCentra/"}>
       <div className="bg-black min-h-screen text-white select-none selection:bg-emerald-500 selection:text-black">
         <Nav />
         <Routes>
--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/presscentra/',
+  base: '/PressCentra/',
   server: {
     port: 6767
   }
