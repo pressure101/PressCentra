be4ec8d0f46ce27192fb334abd11123159f01e7f
Cavasier Morris <cavamorr@gmail.com>
2026-09-21 19:43:09 -0500

    adding dynamic asset location

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PressCentra/',
+  base: process.env.CF_PAGES === '1' 
+    ? '/' 
+    : '/PressCentra/',
   server: {
     port: 6767
   }
