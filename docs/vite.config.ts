a281d75aacb5e6e3f0e67a82f8a7bf14173eb023
Cavasier Morris <cavamorr@gmail.com>
2026-09-21 20:17:11 -0500

    import env vars for url dynamics

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  server: {
-    port: 6767
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL || '/PressCentra/',
+    server: {
+      port: 6767
+    }
   }
 })
