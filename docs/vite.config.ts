8c9466f9da685e5e3dc9c05a61cb1f9c6d15bde7
Cavasier Morris <cavamorr@gmail.com>
2026-09-21 19:55:14 -0500

    adding relative asset location

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.CF_PAGES === '1' 
-    ? '/' 
-    : '/PressCentra/',
+  base: './',
   server: {
     port: 6767
   }
